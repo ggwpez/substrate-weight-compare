27221955b9a8d675ee9010985b37d9bab17fc083
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-05-17 21:53:23 +0200

    0.4.1 (#15)
    * Fix toolchain
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Use comfytables
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Add benches
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * fmt
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Only run docker on success CI
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * v0.4.1
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * lockfile
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * fmt
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Fix CI
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Fix CI
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Fix CI
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Fix CI
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

---
+++ core/benches/parse.rs
@@ -0,0 +1,36 @@
+//! Measures the throughput of parsing an average extrinsic.
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use std::path::Path;
+
+use swc_core::parse::{pallet::parse_file as parse_pallet, storage::parse_file as parse_storage};
+
+fn bench_parse_pallet(c: &mut Criterion) {
+	let path = Path::new("../test_data/new/pallet_staking.rs.txt");
+	let num_ext = parse_pallet(path).expect("Must work").len();
+	let mut group = c.benchmark_group("Parse");
+
+	group.sample_size(100);
+	group.throughput(Throughput::Elements(num_ext as u64));
+	group.bench_function("Pallet.Extrinsic", |b| {
+		b.iter(|| parse_pallet(black_box(path)).expect("Must work"))
+	});
+}
+
+fn bench_parse_storage(c: &mut Criterion) {
+	let path = Path::new("../test_data/new/rocksdb_weights.rs.txt");
+	let mut group = c.benchmark_group("Parse");
+
+	group.sample_size(100);
+	group.throughput(Throughput::Elements(1));
+	group.bench_function("Storage", |b| {
+		b.iter(|| parse_storage(black_box(path)).expect("Must work"))
+	});
+}
+
+criterion_group! {
+	name = benches;
+	config = Criterion::default();
+	targets = bench_parse_pallet, bench_parse_storage
+}
+criterion_main!(benches);
