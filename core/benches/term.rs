7295eeb0f12424b494ade5e9484dd5d198a6869b
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-05-23 17:28:36 +0200

    Add term benches
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

---
+++ core/benches/term.rs
@@ -0,0 +1,31 @@
+//! Measures the throughput of parsing an average extrinsic.
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use std::path::Path;
+
+use swc_core::{term::*, scope::Scope, mul, add, var, val};
+
+fn bench_term_add(c: &mut Criterion) {
+	let mut group = c.benchmark_group("Term");
+	let heigh = 14_u64;
+	let size = 1 << heigh;
+
+	let mut term = add!(var!("x"), var!("y"));
+	for _ in 0..heigh {
+		term = add!(term.clone(), term);
+	}
+	let scope = Scope::empty().with_var("x", val!(245234))
+							  .with_var("y", val!(245231));
+
+	group.throughput(Throughput::Elements(size as u64));	
+	group.bench_function("Add", |b| {
+		b.iter(|| term.eval(&scope).expect("must work"))
+	});
+}
+
+criterion_group! {
+	name = benches;
+	config = Criterion::default();
+	targets = bench_term_add
+}
+criterion_main!(benches);
